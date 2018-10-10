0b050f8c4c0eac38f5e9bbd0fe3b921d2113a657
Ujjwaldhiman <44018161+Ujjwaldhiman@users.noreply.github.com>
2018-10-10 13:06:48 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-# define punctuation
+# define punctuation 
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
 my_str = "Hello!!!, he said ---and went."
@@ -6,7 +6,7 @@ my_str = "Hello!!!, he said ---and went."
 # To take input from the user
 # my_str = input("Enter a string: ")
 
-# remove punctuation from the string
+# remove punctuation from the string and show
 no_punct = ""
 for char in my_str:
    if char not in punctuations:
